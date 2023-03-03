be292beae3e7db32d62f87e69439608e1c87ca3c
Ketul <ketulm8@gmail.com>
2023-03-03 06:14:04 +0530

    Setup NotesViewModel

--- app/src/main/java/com/example/pl_notes_app/feature_note/presentation/notes/NotesState.kt
+++ app/src/main/java/com/example/pl_notes_app/feature_note/presentation/notes/NotesState.kt
@@ -1,3 +1,11 @@
 package com.example.pl_notes_app.feature_note.presentation.notes
 
-data class NotesState()
+import com.example.pl_notes_app.feature_note.domain.model.Note
+import com.example.pl_notes_app.feature_note.domain.util.NoteOrder
+import com.example.pl_notes_app.feature_note.domain.util.OrderType
+
+data class NotesState(
+    val notes: List<Note> = emptyList(),
+    var noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    val isOrderSectionVisible: Boolean = false
+)
